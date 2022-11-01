a6f7a42e81f551c3954191e13bc5133be85b60d7
Lydia Ego <lydia.ego@cyphercrescent.com>
2022-11-01 11:06:40 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+const url = 'https://dummyjson.com/todos/1';
+
+interface Todo {
+    id: number;
+    todo: string;
+    completed: boolean;
+}  //defines structure of an object
+
+axios.get(url).then(res =>{
+    const todo = res.data as Todo;
+    
+    const id = todo.id
+    const title = todo.todo
+    const complete = todo.completed
+
+    console.log(`Todo has id ${id} has a title of ${title} is it complete: ${complete}`);
+    
+})
